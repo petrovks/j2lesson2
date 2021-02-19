7d54ed9d15aa69176d2002a993333dd49cec1c65
petrovks <38166811+petrovks@users.noreply.github.com>
2021-02-19 23:12:16 +0300

    Update MyArraySizeException.java

--- src/main/java/geekbrains/j2lesson2/MyArraySizeException.java
+++ src/main/java/geekbrains/j2lesson2/MyArraySizeException.java
@@ -1,9 +1,7 @@
 package geekbrains.j2lesson2;
 
 public class MyArraySizeException extends ArrayIndexOutOfBoundsException{
-
     public MyArraySizeException(String message) {
         super(message);
     }
-
 }
